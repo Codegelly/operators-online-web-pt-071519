9adf3fb60cb9f094d36decb63ebe596dc7a44fcf
Angelica  Thompson <i.thompson89@outlook.com>
2019-07-30 23:01:33 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,33 @@
 def unsafe?(speed)
+<<<<<<< HEAD
 if speed.between?(40,60)
   false
   else
+=======
+speed.between? == 40,60
+  if speed > 60
+    true
+  else speed < 40 
+>>>>>>> 049b61ad298dcf066371bd571d7cf5c2124f1c8f
     true
   end
 end
 
 
 
+<<<<<<< HEAD
 def not_safe?(speed)
   speed.between?(40,60)? false: true
 #speed > 60 ? true : false 
 #speed < 40 ? true : false 
 end
   
+=======
+#def not_safe?(speed)
+#speed > 60 ? true : false 
+#speed < 40 ? true : false 
+#end
+#	end 
+>>>>>>> 049b61ad298dcf066371bd571d7cf5c2124f1c8f
 
 
